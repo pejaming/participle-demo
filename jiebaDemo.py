5b572bb9134edcb3382bef1b65e51fd76cb88e1b
pejaming <lnz1107@126.com>
2018-06-18 16:15:05 +0800

    second

--- jiebaDemo.py
+++ jiebaDemo.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
     # word=vectorizer.get_feature_names()#获取词袋模型中的所有词语
     # weight=tfidf.toarray()#将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
     # for i in range(len(weight)):#打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
-    #     print( u"-------这里输出第",i,u"类文本的词语tf-idf权重------")
+    #     print( u"-------这里输2出第",i,u"类文本的词语tf-idf权重------")
     #     for j in range(len(word)):
     #         print (word[j],weight[i][j])
